9c3a4493425636f0317f50bbf8583eefe6c00815
Barnabás Gőz <goozbarnabas@gmail.com>
2022-03-26 20:56:33 +0100

    update-prod-test1

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div className="w-screen min-h-screen flex justify-center p-2 font-poppins">
+    <div className="w-full lg:w-5/12 border-8 border-black rounded-lg min-h-full p-2 shadow-3xl">
+      <Component {...pageProps} />
+    </div>
+  </div>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ function Home() {
         <Head>
           <title>ReHal játékszabály(ok)</title>
         </Head>
-        <div className="w-screen flex flex-col justify-start items-center">
+        <div className="w-full flex flex-col justify-start items-center">
             <Image width={187.5} height={51} src={Logo} alt="The ReHal logo"></Image>
             <h1 className="text-3xl font-bold">Játékszabályok</h1>
             <p className="w-full">
@@ -17,16 +17,18 @@ function Home() {
                 <br/>
                 Először is kérlek engedd meg nekünk, hogy megköszünjük, azt, hogy iskolánkat egy lépéssel közelebb jutattad egy új zongora vásárlásának lehetőségéhez.
                 <br/>
-                Minőségi tartalmakért figyelmedbe ajánljuk az <a className="underline decoration-blue-500 decoration-wavy text-blue-500" href="https://www.instagram.com/rehal_e5vos/">Instagram oldalunkat</a>.
+                Minőségi tartalmakért figyelmedbe ajánljuk az <a className="underline decoration-black decoration-wavy text-black" href="https://www.instagram.com/rehal_e5vos/">Instagram oldalunkat (@rehal_e5vos)</a>.
                 <br/>
-                Játékunkat több mint 3 (három) módon játszatod:
+                Játék előtt fontos eldönteni, hogy ki fog kezdeni. Azt javasoljuk, hogy erre használjátok a &quot;Ki evett utoljára halat?&quot; módszer. Játékunkat több mint 3 (három) módon játszatod:
             </p>
             <ul>
-                <li><Link href="/alehetoseg">&quot;A&quot; lehetőség</Link></li>
-                <li><Link href="/blehetoseg">&quot;B&quot; lehetőség</Link></li>
-                <li><Link href="/clehetoseg">&quot;C&quot; lehetőség</Link></li>
+                <li className="underline decoration-wavy">- <Link href="/alehetoseg">&quot;A&quot; lehetőség</Link></li>
+                <li className="underline decoration-wavy">- <Link href="/blehetoseg">&quot;B&quot; lehetőség</Link></li>
+                <li className="underline decoration-wavy">- <Link href="/clehetoseg">&quot;C&quot; lehetőség</Link></li>
             </ul>
-            <h1 className="text-red-600">Ez az online szabálykönyv alpha verziója! Ennél szebb lesz (talán).</h1>
+            <p className="w-full">Ha ezeket elolvastad (vagy nem), akkor mi el is köszönnénk tőled. Szeretnénk jó szórakozást és jó játékot kívánni neked és társaidnak!<br/><br/> Sok szeretettel, az Eötvös József Gimnázium 2021/22 évfolyamának &quot;B&quot; osztályának ReHal csapata.</p>
+            <br/>
+            <p className="w-full">UI.: A kártyán lévő aláírások egyszer még sokat érhetnek, javasojuk, hogy tartsd meg!</p>
         </div>
       </>
   )
